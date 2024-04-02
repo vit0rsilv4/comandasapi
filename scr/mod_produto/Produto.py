989876e6d0d5b2c66eda0e0a0d2b92194c7e69ad
Vit0rSilv4 <vitordmvb@uniplaclages.edu.br>
2024-04-02 20:29:47 -0300

    OUTRO

--- scr/mod_produto/Produto.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Produto(BaseModel):
-    id_Produto: int = None
-    nome_produto: str
-    matricula: str
-    fabricacao: str
-    validade: str = None
-    grupo: int
